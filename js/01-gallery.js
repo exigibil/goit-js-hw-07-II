adcc19021347e56c0370322db81bf4d225834ae8
exigibil <adrian.topa04@gmail.com>
2024-01-19 16:33:14 +0200

    finish hm 7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,49 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+import basicLightbox from "https://cdn.jsdelivr.net/npm/basiclightbox@5.0.4/+esm";
+const modal = basicLightbox.create("<img>");
+const modalImg = modal.element().querySelector("img");
 
-console.log(galleryItems);
+const modalDescription = document.createElement("p");
+modal.element().appendChild(modalDescription);
+
+const gallery = document.querySelector("ul.gallery");
+const fragment = new DocumentFragment();
+const liTemplate = document.createElement("template");
+
+liTemplate.innerHTML = `<li 
+class="gallery__item">
+<a class="gallery__link" 
+href="large-image.jpg"><img 
+class="gallery__image" 
+src="small-image.jpg" 
+data-source="large-image.jpg"
+alt="Image description"/>
+</a></li>`;
+
+function addGalleryItem(item) {
+  const clone = liTemplate.content.cloneNode(true);
+  const img = clone.querySelector("img");
+  img.src = item.preview;
+  img.alt = item.description;
+  img.dataset.source = item.original;
+  fragment.appendChild(clone);
+}
+
+galleryItems.forEach(addGalleryItem);
+gallery.appendChild(fragment);
+
+gallery.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  modalImg.src = e.target.dataset.source;
+  modalDescription.textContent = e.target.alt;
+  modal.show();
+});
+
+gallery.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" && modal.visible()) {
+    e.preventDefault();
+    modal.close();
+  }
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+import SimpleLightbox from "simplelightbox"
+
+const gallery = document.querySelector("ul.gallery");
+const fragment = new DocumentFragment();
+const liTemplate = new DOMParser().parseFromString(`<li class="gallery__item">
+   <a class="gallery__link" href="large-image.jpg">
+      <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+   </a>
+</li>`, "text/html").body.firstElementChild;
+
+function appendFromGalleryItem(item) {
+  const clone = liTemplate.cloneNode(true);
+  const img = clone.querySelector("img");
+  const a = clone.querySelector("a");
+
+  img.src = item.preview;
+  img.alt = item.description;
+  a.href = item.original;
+  fragment.appendChild(clone);
+}
+
+galleryItems.forEach(appendFromGalleryItem);
+gallery.appendChild(fragment);
+new SimpleLightbox('ul.gallery a', { captionsData: "alt", captionDelay: 250 })
